8f588a95b5ca1dba216df647164e15137d2d469d
sunil <sunil.jp.gupta@gmail.com>
2022-06-02 22:20:47 +0530

    first change

---
+++ src/test/java/com/example/spring/datajpa/TestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
